fdb630b4f5acfa0a02a2750f135d6fbc61745cb0
Alfredo Vaz <48705006+AlfredoVaz@users.noreply.github.com>
2020-09-12 15:16:13 -0300

    Alterações

---
+++ src/pages/BuscaCep.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css'
+
+export default class BuscaCep extends React.Component{
+  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        cidade: "",
+        estado: "",
+        endereco: "",
+        faixaDeNumero: ""
+    }
+  }
+
+  buscarCep() {
+      console.log('Chegou aqui')
+      let cep = document.getElementById('cep');
+     
+      let cepParaPesquisa = 'https://viacep.com.br/ws/' + cep.value + '/json'; 
+      axios.get(cepParaPesquisa)
+      .then(resp => {
+        this.setState({cidade: resp.data.localidade});
+        this.setState({estado: resp.data.uf})
+        this.setState({endereco: resp.data.logradouro})
+        this.setState({faixaDeNumero: resp.data.complemento})
+      });
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <head>
+          <script src="./script/jquery-3.1.1.min.js"></script>
+          <script src="./script/bootstrap.min.js"></script>
+          <link rel="stylesheet" href="./script/bootstrap.min.css"></link>
+          <title>Buscar Endereços</title>
+        </head>
+        <h1>Página que faz consultas na API Via Cep</h1>
+          <div class="divGeral">
+              <div class="column">
+                  <label for="">Escolha um CEP que deseja informações</label>
+                  <input type="text" placeholder="Digite um CEP válido" id="cep" data-mask="00000000" class="margemInput form-control" ></input>
+                  <span id="mensagemValidacao"></span>
+                  <button id="botaoEnviar" onClick={() => this.buscarCep()} class="styleButton btn-primary btn">Buscar</button>
+              </div>
+      
+              <div class="column">
+                  <label for="">Cidade</label>
+                  <input type="text" id="localidade" value={this.state.cidade} class="margemInput form-control" ></input>
+                  <label for="">Estado</label>
+                  <input type="text" id="estado" value={this.state.estado} class="margemInput form-control" ></input>
+                  <label for="">Endereço</label>
+                  <input type="text" id="logradouro" value={this.state.endereco} class="margemInput form-control" ></input>
+                  <label for="">Faixa de Numero</label>
+                  <input type="text" id="complemento" value={this.state.faixaDeNumero} class="margemInput form-control" ></input>
+                  <a href="/" id="sair">Sair</a>
+              </div>
+          </div>
+      </div>
+    );
+  }
+}
---
+++ src/pages/Login.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import axios from 'axios'
+import './stylesLogin.css'
+
+export default class Login extends React.Component {
+
+    state = {
+        usuarios: [],
+        login: "",
+        senha: "",
+        loginC: "",
+        senhaC: "",
+        confirmarSenha: "",
+        erro: ""
+    };
+
+
+//certo
+
+    cliqueLogin = async e => {
+        e.preventDefault();
+        const { login, senha } = this.state;
+
+        console.log(login)
+        console.log(senha)
+        if (!login || !senha) {
+            this.setState({ error: "Preencha todos os dados para logar" });
+        } else {
+            try {
+                await axios.post(`https://reqres.in/api/login`, {
+                    email: login,
+                    password: senha
+                }, {
+                    crossDomain: true
+                }).then(result => {
+                    if (result.request.status === 200){
+                        alert("Login efetuado com sucesso")
+                        this.props.history.push("/buscaCep")
+                    }
+                    else (alert("Login ou senha inválidos"))
+                })
+
+            } catch (err) {
+                this.setState({
+                    error:
+                        "Houve um problema com o login, verifique suas credenciais. T.T"
+                });
+            }
+        }
+
+    }
+
+    cliqueCadastro = async e => {
+        e.preventDefault();
+        const { loginC, senhaC, confirmarSenha } = this.state;
+
+        if(senhaC === confirmarSenha) {
+            if (!loginC || !senhaC) {
+                this.setState({ error: "Preencha todos os dados para se cadastrar" });
+            } else {
+                try {
+                    await axios.post(`https://reqres.in/api/register`, {
+                        email: loginC,
+                        password: senhaC
+                    }, {
+                        crossDomain: true
+                    }).then(result => {
+                        console.log(result)
+                        if (result.request.status === 200){
+                            alert("Usuário cadastrado, efetue o login")
+                        }
+                        else (alert("Erro ao cadastrar"))
+                    })
+    
+                } catch (err) {
+                    this.setState({
+                        error:
+                            "Houve um problema com o login, verifique suas credenciais. T.T"
+                    });
+                }
+            }
+        } else {
+            alert('Senhas não conferem')
+        }
+    }
+
+    render() {
+
+        return (
+            <>
+                <div>
+                    <form id="fmLogin" class="container" onSubmit={this.cliqueLogin}>
+                        <div>
+                            <input class="form-control" type="text" name="login" placeholder="Login" value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
+                            <input class="form-control" type="password" name="senha" placeholder="Senha" value={this.state.senha} onChange={e => this.setState({ senha: e.target.value })} />
+                            <a className="botoes"><button class="btn btn-outline-primary" type="submit">Logar</button></a>
+                        </div>
+                    </form>
+
+                    <form id="fmLogin" class="container" onSubmit={this.cliqueCadastro}>
+                        <input class="form-control" type="text" name="login" placeholder="Login" value={this.state.loginC} onChange={e => this.setState({ loginC: e.target.value })} />
+                        <input class="form-control" type="password" name="senha" placeholder="Senha" value={this.state.senhaC} onChange={e => this.setState({ senhaC: e.target.value })} />
+                        <input class="form-control" type="password" name="senha" placeholder="Senha" value={this.state.confirmarSenha} onChange={e => this.setState({ confirmarSenha: e.target.value })} />
+                        <a className="botoes"><button  class="btn btn-outline-secondary" type="submit">Cadastrar</button></a>
+                    </form>
+                </div>
+            </>
+        )
+    }
+}
